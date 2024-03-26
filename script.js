e71d0aa8500155789d0c5c24996ac1447ff28464
KunjanThakor <kunjan7634@gmail.com>
2024-03-26 10:33:36 +0530

    file creation

---
+++ script.js
@@ -0,0 +1,39 @@
+const blackbox = document.querySelector('.blackbox');
+const yes = document.querySelector('.yes');
+
+yes.addEventListener('click', () => {
+    var responseText = document.getElementById("response");
+    responseText.classList.remove("hidden");
+})
+
+blackbox.addEventListener("mouseover", (event) => {
+    let tp = parseInt(Math.random() * (50 - 1) + 1);
+    let lp = parseInt(Math.random() * (50 - 1) + 1);
+    console.log(event.offsetX)
+    if (event.offsetX === tp) {
+        tp = parseInt(Math.random() * (50 - 1) + 1);
+    }
+    else if (event.offsetY === lp) {
+        lp = parseInt(Math.random() * (50 - 1) + 1);
+    } else {
+        blackbox.style.top = `${tp}%`;
+        blackbox.style.left = `${lp}%`;
+    }
+});
+
+
+
+blackbox.addEventListener("mousemove", (event) => {
+    let tp = parseInt(Math.random() * (50 - 1) + 1);
+    let lp = parseInt(Math.random() * (50 - 1) + 1);
+    console.log(event.offsetX)
+    if (event.offsetX === tp) {
+        tp = parseInt(Math.random() * (50 - 1) + 1);
+    }
+    else if (event.offsetY === lp) {
+        lp = parseInt(Math.random() * (50 - 1) + 1);
+    } else {
+        blackbox.style.top = `${tp}%`;
+        blackbox.style.left = `${lp}%`;
+    }
+});
