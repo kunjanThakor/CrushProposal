5e864d21fb261d2cd6d72c6c912b883bbc70c75f
KunjanThakor <kunjan7634@gmail.com>
2024-03-27 09:02:27 +0530

    updated UI

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// import Typed from 'typed.js';
+
 const blackbox = document.querySelector('.blackbox');
 const yes = document.querySelector('.yes');
 
 yes.addEventListener('click', () => {
     var responseText = document.getElementById("response");
     responseText.classList.remove("hidden");
+    blackbox.classList.add("hidden")
+    yes.classList.add("hidden")
     // responseText.classList.toggle("hidden");
 })
 
@@ -37,4 +41,9 @@ blackbox.addEventListener("mousemove", (event) => {
         blackbox.style.top = `${tp}%`;
         blackbox.style.left = `${lp}%`;
     }
+});
+
+var typed = new Typed('.element', {
+    strings: ["Taking a Chance: Will You Be My Special Someone?"],
+    typeSpeed: 30
 });
